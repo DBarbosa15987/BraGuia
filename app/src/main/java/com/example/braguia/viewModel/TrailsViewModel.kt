97f91ebc91c2dc72df5109eeb935dc1241578254
DBarbosa15987 <dbarbosa15987@gmail.com>
2024-04-25 19:41:07 +0100

    update do viewMode (temporário)

--- app/src/main/java/com/example/braguia/viewModel/TrailsViewModel.kt
+++ app/src/main/java/com/example/braguia/viewModel/TrailsViewModel.kt
@@ -11,19 +11,62 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.braguia.BraGuiaApplication
-import com.example.braguia.model.Content
+import com.example.braguia.model.AppInfo
+import com.example.braguia.model.Media
 import com.example.braguia.model.Trail
+import com.example.braguia.network.LoginRequest
+import com.example.braguia.repositories.AppInfoRepository
 import com.example.braguia.repositories.TrailRepository
+import com.example.braguia.repositories.UserRepository
 import kotlinx.coroutines.launch
-import kotlinx.serialization.json.Json
+import okhttp3.Response
+import okhttp3.ResponseBody
+import retrofit2.Call
 
-class TrailsViewModel(private val trailRepository: TrailRepository) : ViewModel() {
+class TrailsViewModel(
+    val trailRepository: TrailRepository,/*FIXME SO POR ENQUANTO*/
+    val appInfoRepository: AppInfoRepository,
+    val userRepository: UserRepository
+) : ViewModel() {
 
-    var homeUiState: HomeUiState by mutableStateOf(HomeUiState())
+    var homeUiState: HomeUiState by mutableStateOf(
+        HomeUiState(
+            appInfo =
+            AppInfo(
+                "", "", listOf(), listOf(),
+                listOf(), ""
+            )
+        )
+    )
         private set
 
     init {
+
+        login()
+    }
+
+/*    fun login(): ResponseBody? {
+
+        var response:ResponseBody? = null
+        viewModelScope.launch {
+
+            val loginRequest: LoginRequest =
+                LoginRequest("premium_user", "premium@email.com", "paiduser")
+            response = userRepository.login(loginRequest)
+            Log.i("LOGIN",response)
+
+        }
+
+        return response
+    }*/
+
+    fun login(){
+
+        //Logica de login
         getTrails()
+        getAppInfo()
+
+
     }
 
     fun getTrails() {
@@ -31,24 +74,24 @@ class TrailsViewModel(private val trailRepository: TrailRepository) : ViewModel(
         viewModelScope.launch {
             homeUiState = try {
                 val result = trailRepository.getTrails()
-                HomeUiState(result, listOf())
+                HomeUiState(result, homeUiState.mediaList, homeUiState.appInfo)
             } catch (e: Exception) {
                 Log.e("TRAILS", e.toString())
-                HomeUiState(listOf())
+                HomeUiState(homeUiState.trailList, homeUiState.mediaList, homeUiState.appInfo)
             }
         }
-
     }
 
-    fun getContent() {
+    fun getAppInfo() {
 
         viewModelScope.launch {
             homeUiState = try {
-                val response = trailRepository.getContent()
-                HomeUiState(listOf(), response)
+                val response = appInfoRepository.getAppInfo()
+                HomeUiState(homeUiState.trailList, homeUiState.mediaList, response)
             } catch (e: Exception) {
-                Log.e("CONTENT", e.toString())
-                HomeUiState(listOf())
+                Log.e("APPINFO", e.toString())
+                //Log.e("APPINFO", e.stackTraceToString())
+                HomeUiState(homeUiState.trailList, homeUiState.mediaList, homeUiState.appInfo)
             }
         }
 
@@ -60,17 +103,18 @@ class TrailsViewModel(private val trailRepository: TrailRepository) : ViewModel(
                 val application =
                     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as BraGuiaApplication)
                 val trailRepository = application.container.trailRepository
-                //application.database.trailDAO()
-                TrailsViewModel(trailRepository = trailRepository)
+                val appInfoRepository = application.container.appInfoRepository
+                val userRepository = application.container.userRepository
+                TrailsViewModel(trailRepository, appInfoRepository, userRepository)
             }
         }
     }
-
 
 }
 
 data class HomeUiState(
     val trailList: List<Trail> = listOf(),
-    val contentList: List<Content> = listOf(),
+    val mediaList: List<Media> = listOf(),
+    val appInfo: AppInfo,
     val test: String = "bomdia"
 )
