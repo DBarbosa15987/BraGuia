95c5cf4c1639feda603b133f2754e22309dfdd13
tsousa111 <tiagao2001@hotmail.com>
2024-04-25 23:14:50 +0100

    init ui structure

---
+++ app/src/main/java/com/example/braguia/ui/TrailListScreen.kt
@@ -0,0 +1,22 @@
+package com.example.braguia.ui
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import com.example.braguia.model.Trail
+import com.example.braguia.ui.components.TrailCard
+
+fun TrailListScreen(trails: List<Trail>){
+
+
+}
+@Composable
+fun TrailList(trails: List<Trail>) {
+    LazyColumn(contentPadding = PaddingValues(10.dp)) {
+        items(trails) {
+            TrailCard(trail = it)
+        }
+    }
+}
