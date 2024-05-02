3b1b2f610c73a875300f2c96943b23f5920e77be
tsousa111 <tiagao2001@hotmail.com>
2024-05-02 17:21:41 +0100

    login implemented needs testing and fix userViewModel

--- app/src/main/java/com/example/braguia/repositories/UserRepository.kt
+++ app/src/main/java/com/example/braguia/repositories/UserRepository.kt
@@ -1,9 +1,8 @@
 package com.example.braguia.repositories
 
+import android.util.Log
 import com.example.braguia.network.API
 import com.example.braguia.network.LoginRequest
-import com.google.gson.JsonObject
-import okhttp3.Response
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
@@ -12,29 +11,35 @@ class UserRepository(
     private val API: API
 ) {
 
-    suspend fun login(loginRequest: LoginRequest) {
-
-        /*val call = API.login(loginRequest)
+    suspend fun login(loginRequest: LoginRequest) : Boolean {
+        var successful = false
+        val call = API.login(loginRequest)
         call.enqueue(object : Callback<ResponseBody> {
-
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                if (response.isSuccessful) {
-                    val jsonResponse = response.body()
-                    // Handle successful login
-                    // Access JSON properties using jsonResponse.getAs<Type>("property_name")
-                } else {
-                    // Handle failed login
-                    val errorMessage = response.errorBody()?.string()
-                    // Parse error message
-                }
-            }
-
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                 // Handle network errors
+                throw t
+            }
+
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: retrofit2.Response<ResponseBody>
+            ) {
+                if (response.code() == 200){
+                    // login is successful
+                    // get cookies and store them with cookie store
+                    Log.i("LOGIN","Login successful for user " + loginRequest.username)
+                    successful = true
+                } else if (response.code() == 400) {
+                    // login is unsuccessful
+                    // credentials didnt match any user
+                    Log.i("LOGIN","Login unsuccessful for user " + loginRequest.username + " due to wrong credentials")
+                } else{
+                    Log.i("LOGIN","Login unsuccessful for user " + loginRequest.username + " due to a unknown reason")
+                }
             }
         }
-        )*/
-
+        )
+        return successful
     }
 
 }
--- app/src/main/java/com/example/braguia/viewModel/UserViewModel.kt
+++ app/src/main/java/com/example/braguia/viewModel/UserViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.braguia.viewModel
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.braguia.model.TrailDB
 import com.example.braguia.network.API
 import com.example.braguia.network.LoginRequest
 import com.example.braguia.repositories.UserRepository
+import kotlinx.coroutines.launch
 
 class UserViewModel(
     private val userRepository: UserRepository
@@ -16,10 +19,14 @@ class UserViewModel(
     var userUiState: UserUiState by mutableStateOf(UserUiState())
 
     suspend fun login(username: String, password: String) {
-
-        val loginRequest: LoginRequest = LoginRequest(username = username, password = password)
-        userRepository.login(loginRequest)
-
+        viewModelScope.launch {
+            val loginRequest: LoginRequest = LoginRequest(username = username, password = password)
+            try {
+                userRepository.login(loginRequest)
+            } catch (e: Exception){
+                Log.e("USERVIEWMODEL", "Login exception $e")
+            }
+        }
     }
 
     fun logout() {
@@ -37,10 +44,14 @@ class UserViewModel(
     fun updateBookmarks() {
 
     }
+    fun updateLoggedIn(state: Boolean) {
+       //TODO update login in variable
+    }
 }
 
 data class UserUiState(
     val history: List<TrailDB> = listOf(),
     val bookmarks: List<TrailDB> = listOf(),
+    val loggedIn : Boolean = false,
     val username: String = ""
 )
