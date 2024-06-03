5b721081ae14e1deac217020aa8eec1bd6c052ee
tsousa111 <tiagao2001@hotmail.com>
2024-06-03 17:55:13 +0100

    init db

---
+++ braguia-react-native/src/model/Pin.js
@@ -0,0 +1,11 @@
+export default class Pin {
+  constructor(pin_id, name, desc, lat, lng, alt) {
+    this.pin_id = pin_id;
+    this.name = name;
+    this.desc = desc;
+    this.lat = lat;
+    this.lng = lng;
+    this.alt = alt;
+  }
+
+}
