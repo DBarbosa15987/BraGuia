bfac0a9e00d870a969c2b62875a32f137d4f7fcb
DBarbosa15987 <dbarbosa15987@gmail.com>
2024-06-05 20:54:55 +0100

    placeholder para um request http

--- braguia-react-native/app/(tabs)/explore.tsx
+++ braguia-react-native/app/(tabs)/explore.tsx
@@ -1,19 +1,72 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform,Text, ScrollView } from 'react-native';
+import { StyleSheet, Image, Platform, Text, ScrollView } from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect, useState } from 'react';
 
+
+const BASE_URL = 'http://192.168.85.186';
+
+// const getAppInfo = async () => {
+//   return fetch(BASE_URL + '/app')
+//     .then(response => {
+//       return response.json();
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
+// };
+
+// async function getAppInfo(){
+//   try {
+//       const response = await fetch(BASE_URL+'/app');
+//       if (!response.ok) {
+//           throw new Error(
+//               `Unable to Fetch Data, Please check URL
+//               or Network connectivity!!`
+//           );
+//       }
+//       const data = await response.json();
+//       return data;
+//   } catch (error) {
+//       console.error('Some Error Occured:', error);
+//   }
+// }
 
 export default function TabTwoScreen() {
+
+
+
+  const [appInfo, setAppInfo] = useState("Loading...")
+  const [testText, setTestText] = useState("")
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(BASE_URL + '/app');
+        if (response.ok) {
+          const data = await response.json();
+          setAppInfo(data);
+          setTestText("bomdia");
+        }
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+      }
+
+    }
+    fetchData();
+  }, []);
+
   return (
     //<ScrollView></ScrollView>
     <ScrollView>
-      
-      <Text>bomdia</Text>
+      <Text>{testText}</Text>
+      <Text>{JSON.stringify(appInfo)}</Text>
+
     </ScrollView>
   );
 }
