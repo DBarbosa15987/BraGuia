a1f83c79ccb099c5a294e22acc2fcdc42313fead
DBarbosa15987 <dbarbosa15987@gmail.com>
2024-06-10 19:09:13 +0100

    user page e settings feitas, mas sem funcionalidade

--- braguia-react-native/app/(tabs)/settings.tsx
+++ braguia-react-native/app/(tabs)/settings.tsx
@@ -1,33 +1,128 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform, Text, ScrollView } from 'react-native';
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { updateAppInfo } from '@/state/actions/appInfo';
-import { fetchAppInfo, fetchTrails } from '../../api/api'
+import { StyleSheet, TouchableOpacity, Switch, Text, ScrollView, View } from 'react-native';
+import { Button, Card, Avatar, Dialog, Portal, Paragraph } from "react-native-paper"
+import { router } from "expo-router";
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { DarkTheme } from '@react-navigation/native';
 
 export default function SettingsPage() {
 
+  const userInfo = useSelector((state) => state.user.info);
+  const [darkMode, setDarkMode] = useState(false);
+  const [delData, setDelData] = useState(false);
+  const [delPrefs, setDelPrefs] = useState(false);
 
-    return (
-        <ScrollView>
-        </ScrollView>
-    );
+  const showDelData = () => setDelData(true);
+  const hideDelData = () => setDelData(false);
+  const showDelPrefs = () => setDelPrefs(true);
+  const hideDelPrefs = () => setDelPrefs(false);
+
+  const handleDelData = () => {
+    // Perform logout logic here
+    console.log('User Data Deleted');
+    setDelData(false);
+  };
+
+  const handleResetPrefs = () => {
+    // Perform logout logic here
+    console.log('User Prefs reset');
+    setDelPrefs(false);
+  };
+
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode);
+    console.log('dark mode switched');
+  };
+
+  const navigateToAbout = () => {
+    router.push("/home/about")
+    console.log('Navigate to about');
+  };
+
+  return (
+    <ScrollView>
+
+      <SettingsCard title="Dark Theme" text="Switch Between Light and Dark Theme" toggle={true} toggleValue={darkMode} toggleAction={handleDarkMode}/>
+      <SettingsCard title="Delete User Data" text="Deletes all user data, including preferences" onPress={showDelData} />
+      <SettingsCard title="Reset Preferences" text="Resets all user preferences" onPress={showDelPrefs} />
+      <SettingsCard title="About BraGuia" text="" onPress={navigateToAbout} />
+
+      <Portal>
+        <Dialog visible={delData} onDismiss={hideDelData}>
+          <Dialog.Title>Delete User Data</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>By pressing confirm, your user data like history, bookmarks and preferences will all be deleted</Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDelData}>Cancel</Button>
+            <Button onPress={handleDelData}>Confirm</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+      <Portal>
+        <Dialog visible={delPrefs} onDismiss={hideDelPrefs}>
+          <Dialog.Title>Reset Preferences</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>By pressing, confirm all your preferences will be reset.</Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDelPrefs}>Cancel</Button>
+            <Button onPress={handleResetPrefs}>Confirm</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+
+    </ScrollView>
+  );
 }
 
+
+const SettingsCard = ({ title, text, onPress=()=>{}, toggle = false ,toggleValue=false, toggleAction=()=>{}}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Card style={styles.card}>
+        <View style={styles.container}>
+          <View style={styles.textContainer}>
+            {text ? (
+              <>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.content}>{text}</Text>
+              </>
+            ) : (
+              <Text style={styles.title}>{title}</Text>
+            )}
+          </View>
+          {toggle ? (
+              <>
+                <Switch value={toggleValue} onValueChange={toggleAction} />
+              </>
+            ) : (<></>)
+            }
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
+};
+
 const styles = StyleSheet.create({
-    headerImage: {
-        color: '#808080',
-        bottom: -90,
-        left: -35,
-        position: 'absolute',
-    },
-    titleContainer: {
-        flexDirection: 'row',
-        gap: 8,
-    },
+  card: {
+    margin: 4,
+    padding: 10,
+  },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  content: {
+    fontSize: 14,
+    marginBottom: 5
+  }
+
 });
--- braguia-react-native/app/(tabs)/user.tsx
+++ braguia-react-native/app/(tabs)/user.tsx
@@ -1,12 +1,121 @@
-import React from "react";
-import { ScrollView, Text } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, Text, View, StyleSheet, Image } from "react-native";
 import { useSelector } from "react-redux";
+import { Button, Card, Avatar,Dialog, Portal, Paragraph } from "react-native-paper"
+
+
 
 export default function UserPage() {
   const userInfo = useSelector((state) => state.user.info);
+  const avatarSrc = userInfo.user_type.toLowerCase() === "premium" ? require('@/assets/images/marega.jpg') : require('@/assets/images/npc.png')
+  const [visible, setVisible] = useState(false);
+
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => setVisible(false);
+
+  const handleLogout = () => {
+    // Perform logout logic here
+    console.log('User logged out');
+    hideDialog();
+  };
+
   return (
     <ScrollView>
-      <Text>{JSON.stringify(userInfo)}</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={avatarSrc}
+          style={styles.profileImage}
+        />
+      </View>
+      <ProfileCard title="Name" icon="account" text={`${userInfo.first_name} ${userInfo.last_name}`} />
+      <ProfileCard title="Username" icon="account" text={userInfo.username} />
+      <ProfileCard title="User Type" icon="account" text={userInfo.user_type} />
+      <ProfileCard title="Last Login" icon="account" text={convertDate(userInfo.last_login)} />
+      <ProfileCard title="Date Joined" icon="account" text={convertDate(userInfo.date_joined)} />
+
+      <Button mode="contained" onPress={showDialog} style={styles.logoutButton}>
+        Logout
+      </Button>
+      <Portal>
+        <Dialog visible={visible} onDismiss={hideDialog}>
+          <Dialog.Title>Confirm Logout</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>Are you sure you want to logout?</Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Cancel</Button>
+            <Button onPress={handleLogout}>Logout</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </ScrollView>
   );
 }
+
+const ProfileCard = ({ title, icon, text }) => {
+  return (
+    <Card style={styles.card}>
+      <View style={styles.container}>
+        <Avatar.Icon size={40} icon={icon} style={styles.icon} />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.content}>{text}</Text>
+        </View>
+      </View>
+    </Card>
+  );
+};
+
+function convertDate(dateString: string) {
+  try {
+    const date: Date = new Date(dateString);
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const year = date.getUTCFullYear();
+    return `${day}-${month}-${year}`;
+  } catch (e) {
+    return dateString;
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    margin: 6,
+    padding: 6,
+  },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    margin: 10,
+  },
+  textContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  content: {
+    fontSize: 14,
+    marginBottom: 10
+  },
+  imageContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginTop: 20,
+
+  },
+  logoutButton: {
+    marginTop: 20,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+  },
+
+});
